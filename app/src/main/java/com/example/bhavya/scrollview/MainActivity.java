02a609fc8c5e233dd24c5f93ba3ab3f2614621ce
Reddy Bhavya Mohana <bhavyamohana@gmail.com>
2019-03-14 16:22:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bhavya/scrollview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bhavya.scrollview;
+
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
